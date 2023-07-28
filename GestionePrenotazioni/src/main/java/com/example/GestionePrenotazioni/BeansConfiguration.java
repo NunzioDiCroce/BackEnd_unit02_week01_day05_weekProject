ea36b7842e7b8bb1a92b89d7725b64b1b3c307a1
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 12:45:33 +0200

    Create BeansConfiguration.java

---
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/BeansConfiguration.java
@@ -0,0 +1,12 @@
+package com.example.GestionePrenotazioni;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+
+@PropertySource("classpath:application.properties")
+
+public class BeansConfiguration {
+
+}
