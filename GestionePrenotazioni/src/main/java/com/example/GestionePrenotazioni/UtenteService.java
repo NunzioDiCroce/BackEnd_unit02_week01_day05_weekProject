2398999c19ee9ca53bc037a747fecfb3ab426761
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 12:39:31 +0200

    Create UtenteService.java

---
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/UtenteService.java
@@ -0,0 +1,21 @@
+package com.example.GestionePrenotazioni;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class UtenteService {
+
+	@Autowired
+	private UtenteRepository utenteRepository;
+
+	public void save(Utente _utente) {
+		utenteRepository.save(_utente);
+		log.info("Utente con ID " + _utente.getId() + " salvato con successo");
+
+	}
+
+}
