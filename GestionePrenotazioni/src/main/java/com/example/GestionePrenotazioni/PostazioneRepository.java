02051fa8ac4bd087b6a3fa8855b4ac883ad280bc
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 12:27:27 +0200

    Create EdificioRepository.java

--- GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/PostazioneRepository.java
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/PostazioneRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface PrenotazioneRepository extends JpaRepository<Prenotazione, Long> {
+public interface PostazioneRepository extends JpaRepository<Postazione, Long> {
 
 }
