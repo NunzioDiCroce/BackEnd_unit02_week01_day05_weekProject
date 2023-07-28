85b1405642860cf77ab108a0156043af3487c947
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 15:02:28 +0200

    Update PostazioneRepository.java

--- GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/PostazioneRepository.java
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/PostazioneRepository.java
@@ -1,5 +1,6 @@
 package com.example.GestionePrenotazioni;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +15,12 @@ public interface PostazioneRepository extends JpaRepository<Postazione, Long> {
 	@Query("SELECT p FROM Postazione p WHERE p.tipoPostazione = :tipoPostazione AND p.edificio.citta = :citta")
 	List<Postazione> cercaPostazionePerTipoAndCitta(@Param("tipoPostazione") TipoPostazione tipoPostazione,
 			@Param("citta") String citta);
+
+	// - - - - - - - - - - - - - - - CUSTOM QUERY
+	@Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END " + "FROM Prenotazione pr " + "JOIN pr.postazione p "
+			+ "WHERE p.id = :postazioneId " + "AND pr.inizioPrenotazione <= :dataFine "
+			+ "AND pr.finePrenotazione >= :dataInizio")
+	boolean postazioneLibera(@Param("postazioneId") Long postazioneId, @Param("dataInizio") LocalDate dataInizio,
+			@Param("dataFine") LocalDate dataFine);
 
 }
