4bd59345869c39d021036a575096f6c39b4d7af7
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 12:43:02 +0200

    Create PrenotazioniRunner.java

---
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/PrenotazioniRunner.java
@@ -0,0 +1,24 @@
+package com.example.GestionePrenotazioni;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@Slf4j
+
+public class PrenotazioniRunner implements CommandLineRunner {
+	@Override
+	public void run(String... args) throws Exception {
+		log.info("Runner is running...");
+
+		configWithAnnotationConfig();
+
+	}
+
+	public static void configWithAnnotationConfig() {
+
+	}
+
+}
