75fac659aa9508baecb6b1088eed9d03f787dfc6
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 11:57:39 +0200

    Update Utente.java

--- GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/Utente.java
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/Utente.java
@@ -1,5 +1,37 @@
 package com.example.GestionePrenotazioni;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "utenti")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
+@Builder
+
 public class Utente {
+
+	@Id
+	@GeneratedValue
+	private long id;
+
+	protected String userName;
+	protected String nome;
+	protected String mail;
 
 }
