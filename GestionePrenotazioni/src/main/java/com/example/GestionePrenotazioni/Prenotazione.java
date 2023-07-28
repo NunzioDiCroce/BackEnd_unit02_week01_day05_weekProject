db0053e3adcd1d69917b55074f7bfa2b239ef136
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 12:05:25 +0200

    Update Prenotazione.java

--- GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/Prenotazione.java
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/Prenotazione.java
@@ -3,9 +3,30 @@ package com.example.GestionePrenotazioni;
 import java.time.LocalDate;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "prenotazioni")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
+@Builder
 
 public class Prenotazione {
 
