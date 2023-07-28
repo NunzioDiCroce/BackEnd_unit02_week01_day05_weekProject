bdecb76a49c3ceb28ee59352ad20bf6cee7e099a
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 11:55:15 +0200

    Update Postazione.java

--- GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/Postazione.java
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/Postazione.java
@@ -1,10 +1,31 @@
 package com.example.GestionePrenotazioni;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.extern.slf4j.Slf4j;
+
+@Entity
+@Table(name = "postazioni")
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Slf4j
+
+@Builder
 
 public class Postazione {
 
