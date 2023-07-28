becb1b24ef7a30628cf88d630cb58cdd1847b62f
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-28 12:33:16 +0200

    Create EdificioService.java

---
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/EdificioService.java
@@ -0,0 +1,21 @@
+package com.example.GestionePrenotazioni;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class EdificioService {
+
+	@Autowired
+	private EdificioRepository edificioRepository;
+
+	public void save(Edificio _edificio) {
+		edificioRepository.save(_edificio);
+		log.info(_edificio.getNome() + " con ID " + _edificio.getId() + " salvato con successo");
+
+	}
+
+}
