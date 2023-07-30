a67b4a9323d49225d4878cf4492714b45a0e1488
NunzioDiCroce <nunziodavide@gmail.com>
2023-07-30 18:53:05 +0200

    Update UtenteService.java

--- GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/UtenteService.java
+++ GestionePrenotazioni/src/main/java/com/example/GestionePrenotazioni/UtenteService.java
@@ -1,7 +1,5 @@
 package com.example.GestionePrenotazioni;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,8 +18,8 @@ public class UtenteService {
 
 	}
 
-	public Optional<Utente> findById(long _id) {
-		return utenteRepository.findById(_id);
+	public Utente findById(long _id) throws ItemNotFoundException {
+		return utenteRepository.findById(_id).orElseThrow(() -> new ItemNotFoundException(_id));
 
 	}
 
